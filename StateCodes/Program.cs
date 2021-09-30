8dbb85471a3f8f9ecf08a8742b61548834d4da6e
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-30 10:58:23 -0400

    Add project files.

---
+++ StateCodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;   // must have this using statement in order to use collections
+
+namespace StateCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new List<State>()
+            {
+                new State("OH","Ohio"),
+                new State("KY","Kentucky"),
+                new State("IN", "Indiana"),
+                new State("IL", "Illinois")
+            };
+
+
+            //var state1 = new State("OH","Ohio");
+            //states.Add(state1);
+            //var stateky = new State("KY", "Kentucky");
+            //states.Add(stateky);
+
+            //states.Add( new State("IN", "Indiana") );
+
+            foreach (var state in states)
+            {
+                Console.WriteLine($"The Code is: {state.Code} The State is {state.Name}");
+            }
+
+            //var statecodes = new Dictionary<string, string>();
+            //statecodes.Add("OH", "Ohio");
+            //statecodes.Add("IN", "Indiana");
+            //statecodes.Add("KY", "Kentucky");
+
+            //var ohio = statecodes["OH"];
+            //Console.WriteLine(ohio);
+        }
+    }
+}
---
+++ StateCodes/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCodes
+{
+    class State
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public State(string Code, string Name)
+        {
+            this.Code = Code;
+            this.Name = Name;
+        }
+    }
+}
